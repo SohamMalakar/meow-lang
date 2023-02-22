3c90121f49d35f2ba80fbfd4e9dc9f530265e1b9
Soham Malakar <51132488+SohamMalakar@users.noreply.github.com>
2023-02-22 14:49:36 +0530

    Add support for built-in function overloading

--- src/values/_BuiltInFunction.java
+++ src/values/_BuiltInFunction.java
@@ -37,109 +37,133 @@ public class _BuiltInFunction extends _BaseFunction
 
         String methodName = "execute_" + name;
 
-        Method method = getClass().getDeclaredMethod(methodName, executeCtx.getClass());
+        Method method = getClass().getDeclaredMethod(methodName, executeCtx.getClass(), args.getClass());
 
         String paramMethod = "param_" + name;
-        Method param = getClass().getDeclaredMethod(paramMethod);
+        Method param = getClass().getDeclaredMethod(paramMethod, args.getClass());
 
-        @SuppressWarnings("unchecked") ArrayList<String> argNames = (ArrayList<String>)param.invoke(this);
+        @SuppressWarnings("unchecked") ArrayList<String> argNames = (ArrayList<String>)param.invoke(this, args);
 
         res.register(checkAndPopulateArgs(argNames, args, executeCtx));
 
         if (res.shouldReturn())
             return res;
 
-        _Value returnValue = res.register((RTResult)method.invoke(this, executeCtx));
+        _Value returnValue = res.register((RTResult)method.invoke(this, executeCtx, args));
 
         if (res.shouldReturn())
             return res;
         return res.success(returnValue);
     }
 
-    public ArrayList<String> param_print()
+    public ArrayList<String> param_print(ArrayList<_Value> args) throws Exception
     {
-        return new ArrayList<String>(Arrays.asList("value"));
+        ArrayList<String> params = new ArrayList<String>();
+
+        for (int i = 0; i < args.size(); i++)
+            params.add("value" + i);
+
+        return params;
     }
 
-    public RTResult execute_print(Context execCtx) throws Exception
+    public RTResult execute_print(Context execCtx, ArrayList<_Value> args) throws Exception
     {
-        var value = execCtx.symbolTable.get("value");
-        System.out.print(value.rawValue());
+        _Value value;
+
+        for (int i = 0; i < args.size(); i++)
+        {
+            value = execCtx.symbolTable.get("value" + i);
+            System.out.print(value.rawValue());
+        }
+
         return new RTResult().success(new _None());
     }
 
-    public ArrayList<String> param_input()
+    public ArrayList<String> param_input(ArrayList<_Value> args) throws Exception
     {
-        return new ArrayList<String>();
+        ArrayList<String> params = new ArrayList<String>();
+
+        if (args.size() > 1)
+            throw new Exception("input expected at most 1 argument, got " + args.size());
+        else if (args.size() == 1)
+            params.add("value");
+
+        return params;
     }
 
-    public RTResult execute_input(Context execCtx) throws Exception
+    public RTResult execute_input(Context execCtx, ArrayList<_Value> args) throws Exception
     {
+        if (args.size() == 1)
+        {
+            _Value value = execCtx.symbolTable.get("value");
+            System.out.print(value.rawValue());
+        }
+
         String str = Input.nextLine();
         return new RTResult().success(new _String(str));
     }
 
-    public ArrayList<String> param_str()
+    public ArrayList<String> param_str(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("value"));
     }
 
-    public RTResult execute_str(Context execCtx) throws Exception
+    public RTResult execute_str(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var value = execCtx.symbolTable.get("value");
         return new RTResult().success(new _String(value.rawValue()));
     }
 
-    public ArrayList<String> param_len()
+    public ArrayList<String> param_len(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("list"));
     }
 
-    public RTResult execute_len(Context execCtx) throws Exception
+    public RTResult execute_len(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var value = execCtx.symbolTable.get("list");
         return new RTResult().success(new _Number("int", String.valueOf(value.size())));
     }
 
-    public ArrayList<String> param_int()
+    public ArrayList<String> param_int(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("value"));
     }
 
-    public RTResult execute_int(Context execCtx) throws Exception
+    public RTResult execute_int(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var value = execCtx.symbolTable.get("value");
         return new RTResult().success(new _Number("int", String.valueOf(Integer.parseInt(value.rawValue()))));
     }
 
-    public ArrayList<String> param_float()
+    public ArrayList<String> param_float(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("value"));
     }
 
-    public RTResult execute_float(Context execCtx) throws Exception
+    public RTResult execute_float(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var value = execCtx.symbolTable.get("value");
         return new RTResult().success(new _Number("float", String.valueOf(Double.parseDouble(value.rawValue()))));
     }
 
-    public ArrayList<String> param_bool()
+    public ArrayList<String> param_bool(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("value"));
     }
 
-    public RTResult execute_bool(Context execCtx) throws Exception
+    public RTResult execute_bool(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var value = execCtx.symbolTable.get("value");
         return new RTResult().success(new _Bool(String.valueOf(value.isTrue())));
     }
 
-    public ArrayList<String> param_run()
+    public ArrayList<String> param_run(ArrayList<_Value> args)
     {
         return new ArrayList<String>(Arrays.asList("fn"));
     }
 
-    public RTResult execute_run(Context execCtx) throws Exception
+    public RTResult execute_run(Context execCtx, ArrayList<_Value> args) throws Exception
     {
         var fn = execCtx.symbolTable.get("fn");
 
